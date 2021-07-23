cb085304c74a9e295fe48c037830485379495a97
aprox13 <ifkbhit@gmail.com>
2021-07-23 14:20:45 +0300

    fix

--- src/presenter/oled_screen.cpp
+++ src/presenter/oled_screen.cpp
@@ -6,16 +6,42 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <StringBuilder.h>
 
 #include "common.h"
 #include "string.h"
 
 oled_screen::oled_screen(Adafruit_SSD1306 &screen) : display(screen) {}
 
-void ensure_text_bound(Adafruit_SSD1306 &display, const char *str, uint16_t *w, uint16_t *h)
+void ensure_text_bound(Adafruit_SSD1306 &display, const String &str, uint16_t *w, uint16_t *h)
 {
   int16_t x = 0, y = 0, x1 = 0, y1 = 0;
   display.getTextBounds(str, x, y, &x1, &y1, w, h);
+}
+
+String dtos(double d, int prec)
+{
+  String res = "";
+  res += ((long)d);
+
+  d -= (long)d;
+
+  while (prec > 0)
+  {
+    d *= 10;
+  }
+
+  long ld = (long)d;
+
+  while (ld > 0 && ld % 10 == 0)
+    ld /= 10;
+
+  if (ld != 0)
+  {
+    res += (".");
+    res += (ld);
+  }
+  return res;
 }
 
 void oled_screen::draw_flow_screen(double liters, double liters_per_min, long ticks)
@@ -24,11 +50,9 @@ void oled_screen::draw_flow_screen(double liters, double liters_per_min, long ti
   display.setTextSize(2);
   display.setTextColor(WHITE);
 
-  char *volume_str = new char[20],
-       *speed_str = new char[20],
-       *ticks_str = new char[30];
-
-  fill_flow_string(liters, liters_per_min, ticks, volume_str, speed_str, ticks_str);
+  String volume_str = dtos(liters, 2) + "L";
+  String speed_str = dtos(liters_per_min, 2) + " L/min";
+  String ticks_str = String() + ticks + " ticks";
 
   uint16_t w, h;
   ensure_text_bound(display, volume_str, &w, &h);

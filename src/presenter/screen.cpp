2b9a44400f8bfe4640d3adaba9093ae43bb388f3
aprox13 <ifkbhit@gmail.com>
2021-07-23 11:49:44 +0300

    more logs

--- src/presenter/screen.cpp
+++ src/presenter/screen.cpp
@@ -2,6 +2,7 @@
 #include "presenter/screen.h"
 #include "common.h"
 #include "string.h"
+#include "logging.h"
 
 void fill_flow_string(double liters,
                       double liters_per_min,
@@ -16,11 +17,17 @@ void fill_flow_string(double liters,
   strcpy(volume_str, buff);
   strcat(volume_str, "L");
 
+  PART_LOG("Got volume_str="); LOG(volume_str);
+
   pretty_double2_prec(liters_per_min, buff);
   strcpy(speed_str, buff);
   strcat(speed_str, " L/min");
 
+  PART_LOG("Got speed_str="); LOG(speed_str);
+
   ltoa(ticks, buff, 10);
   strcpy(ticks_str, buff);
   strcat(ticks_str, " ticks");
+
+  PART_LOG("Got ticks_str="); LOG(ticks_str);
 }
